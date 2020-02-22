35344ec5fef00cab82708f8a9d7226e43722b824
Doctor Ake <drakedemuyt@gmail.com>
2020-02-22 17:55:57 -0500

    Clarified variables, added for loop, implemented random function to choose next character.

--- Develop/script.js
+++ Develop/script.js
@@ -16,50 +16,70 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // What I need for this to work
-var specialValue = "!@#$%^&*()_+".split("");
-var numValue = "1234567890".split("");
-var upperValue = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var lowerValue = "abcdefghijklmnopqrstuvwxyz".split("");
+var specialValue = "!@#$%^&*()_+";
+var numValue = "1234567890";
+var upperValue = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerValue = "abcdefghijklmnopqrstuvwxyz";
 var password = "";
 var superArray = "";
 
-prompt("How many characters would you like your password to contain?")
-
+passwordLength = prompt("How many characters would you like your password to contain?")
 specialChar = confirm("Click OK to include special characters.")
-
 numChar = confirm("Click OK to include numeric characters.")
-
 upperChar = confirm("Click OK to include uppercase characters.")
-
 lowerChar = confirm("Click OK to include lowercase characters.")
+var count = 0 
+
 
 if (specialChar == true) {
-  password += specialValue.charAt(Math.floor(Math.random() * (specialValue.length - 1)))
-  superArray += 
+  password += specialValue.charAt(Math.floor(Math.random() * (specialValue.length - 1)));
+  superArray += specialValue;
 }
 if (numChar == true) {
-  passwordArray += numChar.charAt(Math.floor(Math.random() * (numChar.length - 1)))
+  password += numValue.charAt(Math.floor(Math.random() * (numValue.length - 1)));
+  superArray += numValue;
 }
 if (upperChar == true) {
-  passwordArray += upperChar.charAt(Math.floor(Math.random() * (upperChar.length - 1)))
+  password += upperValue.charAt(Math.floor(Math.random() * (upperValue.length - 1)));
+  superArray += upperValue;
 }
 if (lowerChar == true) {
-  passwordArray += lowerChar.charAt(Math.floor(Math.random() * (lowerChar.length - 1)))
+  password += lowerValue.charAt(Math.floor(Math.random() * (lowerValue.length - 1)));
+  superArray += lowerValue;
+}
+else {
+  alert("Please select at least one set of characters to include.")
 }
 
+for(count; count <= passwordLength; count++) {
+  typeChosen=Math.floor(Math.random(1-4));
+  if(typeChosen == 1) {
+    specialValue;
+    count++;
+  }
+  else if (typeChosen == 2) {
+    numValue;
+    count++;
+  }
+  else if (typeChosen == 3) {
+    upperValue;
+    count++;
+  }
+  else if (typeChosen == 4) {
+    lowerValue;
+    count++;
+  }
 
+}
 
-
-for(var i = 0; i<= pasword; i++) {
 //  passwordText = passwordText + specialValue.charAt(Math.floor(Math.random() * Math.floor(specialValue.length - 1)));
-  passwordText = passwordText + specialValue.charAt(Math.floor(Math.random() * (specialValue.length - 1)));
-}
-for(var i = 0; i <= password; i++) {
-  passwordText = passwordText + numValue.charAt(Math.floor(Math.random() * Math.floor(numValue.length - 1)));
-}
-for(var i = 0; i <= password; i++) {
-  passwordText = passwordText + upperValue.charAt(Math.floor(Math.random() * Math.floor(upperValue.length - 1)));
-}
-for(var i = 0; i <= password; i++) {
-  passwordText = passwordText + lowerValue.charAt(Math.floor(Math.random() * Math.floor(lowerValue.length - 1)));
-}
+
+// for(var i = 0; i <= password; i++) {
+//   passwordText = passwordText + numValue.charAt(Math.floor(Math.random() * Math.floor(numValue.length - 1)));
+// }
+// for(var i = 0; i <= password; i++) {
+//   passwordText = passwordText + upperValue.charAt(Math.floor(Math.random() * Math.floor(upperValue.length - 1)));
+// }
+// for(var i = 0; i <= password; i++) {
+//   passwordText = passwordText + lowerValue.charAt(Math.floor(Math.random() * Math.floor(lowerValue.length - 1)));
+// }
