14ac6241b70ea8cad5a685a2dfe2a9b1e22e8175
Doctor Ake <drakedemuyt@gmail.com>
2020-02-22 20:12:20 -0500

    Completed draft with new loops, cleaned code, and comments added.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,89 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var specialValue = "!@#$%^&*()_+";
+  var numValue = "1234567890";
+  var upperValue = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerValue = "abcdefghijklmnopqrstuvwxyz";
+  var password = "";
+  // var superArray = "";
+  var count = 0
+  
+  function randomSpecial() {
+    password += specialValue.charAt(Math.floor(Math.random() * (specialValue.length - 1)));
+  }
+  
+  function randomNum() {
+    password += numValue.charAt(Math.floor(Math.random() * (numValue.length - 1)));
+  }
+  
+  function randomUpper() {
+    password += upperValue.charAt(Math.floor(Math.random() * (upperValue.length - 1)));
+  }
+  
+  function randomLower() {
+    password += lowerValue.charAt(Math.floor(Math.random() * (lowerValue.length - 1)));
+  }
+  
+  passwordLength = prompt("How many characters would you like your password to contain?")
+  
+  do {
+    specialChar = confirm("Click OK to include special characters.")
+    numChar = confirm("Click OK to include numeric characters.")
+    upperChar = confirm("Click OK to include uppercase characters.")
+    lowerChar = confirm("Click OK to include lowercase characters.")
+    if (specialChar != true && numChar != true && upperChar != true && lowerValue != true) {
+      alert("Please select at least one set of characters to include.")
+    }
+  }
+  while (specialChar != true && numChar != true && upperChar != true && lowerValue != true);
+  
+  
+  
+  if (specialChar == true) {
+    randomSpecial();
+    count++;
+    // superArray += specialValue;
+  }
+  if (numChar == true) {
+    randomNum();
+    count++;
+    // superArray += numValue;
+  }
+  if (upperChar == true) {
+    randomUpper();
+    count++;
+    // superArray += upperValue;
+  }
+  if (lowerChar == true) {
+    randomLower();
+    count++;
+    // superArray += lowerValue;
+  }
+  
+  //At least one type needs to be chosen before you can use while loop
+  
+  while (count < passwordLength) {
+    typeChosen = Math.ceil(Math.random() * 4);
+    if (specialChar == true && typeChosen == 1) {
+      randomSpecial();
+      count++;
+    } else if (numChar == true && typeChosen == 2) {
+      randomNum();
+      count++;
+    } else if (upperChar == true && typeChosen == 3) {
+      randomUpper();
+      count++;
+    } else if (lowerChar == true && typeChosen == 4) {
+      randomLower();
+      count++;
+    }
+  
+  }
+  return password;
+}  
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -16,75 +99,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // What I need for this to work
-var specialValue = "!@#$%^&*()_+";
-var numValue = "1234567890";
-var upperValue = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerValue = "abcdefghijklmnopqrstuvwxyz";
-var password = "";
-var superArray = "";
-var count = 0
 
-function randomSpecial() {
-  password += specialValue.charAt(Math.floor(Math.random() * (specialValue.length - 1)));
-}
-
-function randomNum() {
-  password += numValue.charAt(Math.floor(Math.random() * (numValue.length - 1)));
-}
-
-function randomUpper() {
-  password += upperValue.charAt(Math.floor(Math.random() * (upperValue.length - 1)));
-}
-
-function randomLower() {
-  password += lowerValue.charAt(Math.floor(Math.random() * (lowerValue.length - 1)));
-}
-
-passwordLength = prompt("How many characters would you like your password to contain?")
-specialChar = confirm("Click OK to include special characters.")
-numChar = confirm("Click OK to include numeric characters.")
-upperChar = confirm("Click OK to include uppercase characters.")
-lowerChar = confirm("Click OK to include lowercase characters.")
-
-
-
-if (specialChar == true) {
-  randomSpecial();
-  superArray += specialValue;
-}
-if (numChar == true) {
-  randomNum();
-  superArray += numValue;
-}
-if (upperChar == true) {
-  randomUpper();
-  superArray += upperValue;
-}
-if (lowerChar == true) {
-  randomLower();
-  superArray += lowerValue;
-} 
-else {
-  alert("Please select at least one set of characters to include.")
-}
-
-for (count; count <= passwordLength; count++) {
-  typeChosen = Math.ceil(Math.random() * 4);
-  if (typeChosen == 1) {
-    randomSpecial();
-  } else if (typeChosen == 2) {
-    randomNum();
-  } else if (typeChosen == 3) {
-    randomUpper();
-  } else if (typeChosen == 4) {
-    randomLower();
-  } else {
-    alert("Error")
-  }
-
-}
-
-alert(password);
 //  passwordText = passwordText + specialValue.charAt(Math.floor(Math.random() * Math.floor(specialValue.length - 1)));
 
 // for(var i = 0; i <= password; i++) {
