5026fc6408373f03b7d64c248ab526a27fa8838b
Doctor Ake <drakedemuyt@gmail.com>
2020-02-22 20:14:26 -0500

    Completed draft with new loops, cleaned code, and comments added.

--- Develop/script.js
+++ Develop/script.js
@@ -1,69 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Send randomized password to writePassword function.
 function generatePassword() {
   var specialValue = "!@#$%^&*()_+";
   var numValue = "1234567890";
   var upperValue = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerValue = "abcdefghijklmnopqrstuvwxyz";
   var password = "";
-  // var superArray = "";
   var count = 0
-  
+
+  //Sets up intial randomized numbers to adhere to clients' request.
   function randomSpecial() {
     password += specialValue.charAt(Math.floor(Math.random() * (specialValue.length - 1)));
   }
-  
+
   function randomNum() {
     password += numValue.charAt(Math.floor(Math.random() * (numValue.length - 1)));
   }
-  
+
   function randomUpper() {
     password += upperValue.charAt(Math.floor(Math.random() * (upperValue.length - 1)));
   }
-  
+
   function randomLower() {
     password += lowerValue.charAt(Math.floor(Math.random() * (lowerValue.length - 1)));
   }
-  
-  passwordLength = prompt("How many characters would you like your password to contain?")
-  
+
+  //Loop to ensure correct password length.
+  do {
+    passwordLength = prompt("Enter your desired password length between 8 - 128 characters:")
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Please select a value between 8 and 128");
+    }
+  }
+  while (passwordLength < 8 || passwordLength > 128);
+
+  //Loop to ensure at least one type of character is chosen.
   do {
     specialChar = confirm("Click OK to include special characters.")
     numChar = confirm("Click OK to include numeric characters.")
     upperChar = confirm("Click OK to include uppercase characters.")
     lowerChar = confirm("Click OK to include lowercase characters.")
     if (specialChar != true && numChar != true && upperChar != true && lowerValue != true) {
-      alert("Please select at least one set of characters to include.")
+      alert("Please select at least one set of characters to include.");
     }
   }
   while (specialChar != true && numChar != true && upperChar != true && lowerValue != true);
-  
-  
-  
+
+
+  //If statements to call random character functions
   if (specialChar == true) {
     randomSpecial();
     count++;
-    // superArray += specialValue;
   }
   if (numChar == true) {
     randomNum();
     count++;
-    // superArray += numValue;
   }
   if (upperChar == true) {
     randomUpper();
     count++;
-    // superArray += upperValue;
   }
   if (lowerChar == true) {
     randomLower();
     count++;
-    // superArray += lowerValue;
   }
-  
+
   //At least one type needs to be chosen before you can use while loop
-  
+
   while (count < passwordLength) {
     typeChosen = Math.ceil(Math.random() * 4);
     if (specialChar == true && typeChosen == 1) {
@@ -79,10 +84,10 @@ function generatePassword() {
       randomLower();
       count++;
     }
-  
+
   }
   return password;
-}  
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -97,17 +102,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// What I need for this to work
-
-//  passwordText = passwordText + specialValue.charAt(Math.floor(Math.random() * Math.floor(specialValue.length - 1)));
-
-// for(var i = 0; i <= password; i++) {
-//   passwordText = passwordText + numValue.charAt(Math.floor(Math.random() * Math.floor(numValue.length - 1)));
-// }
-// for(var i = 0; i <= password; i++) {
-//   passwordText = passwordText + upperValue.charAt(Math.floor(Math.random() * Math.floor(upperValue.length - 1)));
-// }
-// for(var i = 0; i <= password; i++) {
-//   passwordText = passwordText + lowerValue.charAt(Math.floor(Math.random() * Math.floor(lowerValue.length - 1)));
-// }
